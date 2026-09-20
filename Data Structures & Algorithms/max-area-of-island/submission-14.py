a7684e652a12f82eb83f2ab569906b4886f042e2
georgeagie <136653237+georgeagie@users.noreply.github.com>
2026-09-20 01:09:25 -0700

    Update: max-area-of-island - submission-14

--- Data Structures & Algorithms/max-area-of-island/submission-14.py
+++ Data Structures & Algorithms/max-area-of-island/submission-14.py
@@ -5,19 +5,19 @@ class Solution:
         islands = 0
         rows, cols = len(grid), len(grid[0])
         directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
-        def dfs(r, c):
+        def bfs(r, c):
             grid[r][c] = "0"
             for dr, dc in directions:
                 nr, nc = r + dr, c + dc
                 if nr < 0 or nr >= rows or nc < 0 or nc >= cols:
                     continue
                 if grid[nr][nc] == "1":
-                    dfs(nr, nc)
+                    bfs(nr, nc)
 
         for r in range(rows):
             for c in range(cols):
                 if grid[r][c] == "1":
-                    dfs(r, c)
+                    bfs(r, c)
                     islands += 1
     
         return islands
