1237e42c2665d9f8ac8af02872c31a3833f06cce
georgeagie <136653237+georgeagie@users.noreply.github.com>
2026-09-20 01:09:24 -0700

    Update: max-area-of-island - submission-14

--- Data Structures & Algorithms/max-area-of-island/submission-14.py
+++ Data Structures & Algorithms/max-area-of-island/submission-14.py
@@ -1,31 +1,23 @@
+from collections import deque
+
 class Solution:
-    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
-        ROWS, COLS = len(grid), len(grid[0])
-        DIRECTIONS = [(1,0), (-1,0), (0,1), (0,-1)]
-        max_area = 0
+    def numIslands(self, grid: List[List[str]]) -> int:
+        islands = 0
+        rows, cols = len(grid), len(grid[0])
+        directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+        def dfs(r, c):
+            grid[r][c] = "0"
+            for dr, dc in directions:
+                nr, nc = r + dr, c + dc
+                if nr < 0 or nr >= rows or nc < 0 or nc >= cols:
+                    continue
+                if grid[nr][nc] == "1":
+                    dfs(nr, nc)
 
-        def bfs(x, y):
-            q = deque()
-            count = 1
-            q.append((x, y))
-            grid[x][y] = 0
-
-            while q:
-                r, c = q.popleft()
-                for dr, dc in DIRECTIONS:
-                    new_r, new_c = r + dr, c + dc
-                    if (new_r < 0 or new_c < 0
-                        or new_r >= ROWS or new_c >= COLS
-                        or grid[new_r][new_c] == 0):
-                        continue
-                    q.append((new_r, new_c))
-                    count += 1
-                    grid[new_r][new_c] = 0
-            return count
-        
-        for r in range(ROWS):
-            for c in range(COLS):
-                if grid[r][c] == 1:
-                    max_area = max(max_area, bfs(r, c))
-
-        return max_area
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == "1":
+                    dfs(r, c)
+                    islands += 1
+    
+        return islands
